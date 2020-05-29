7af58fea6dc71eafe338094be9b09016745dfc0a
Kirill <fairsuspect@gmail.com>
2020-05-29 14:37:11 +0300

    Improved physics, scene loading

--- Assets/Scripts/SceneSelectScript.cs
+++ Assets/Scripts/SceneSelectScript.cs
@@ -6,11 +6,27 @@ using UnityEngine.SceneManagement;
 public class SceneSelectScript : MonoBehaviour
 {
     public void selectScene() {
-        switch (this.gameObject.name)
+       /* switch (gameObject.name)
         {
             case "catchUpScene":
                 SceneManager.LoadScene("KlevleevSceneCatchUp");
                 break;
         }
+        */
+        switch(Random.Range(0,3))
+        {
+            case 0:
+                SceneManager.LoadScene("CoinGrab");
+                break;
+            case 1:
+                SceneManager.LoadScene("FallingBlocks");
+                break;
+            case 2:
+                SceneManager.LoadScene("CatchUp");
+                break;
+            default:
+                SceneManager.LoadScene("Menu");
+                break;
+        };
     }
 }
--- Assets/Scripts/movementP1.cs
+++ Assets/Scripts/movementP1.cs
@@ -7,7 +7,7 @@ public class movementP1 : MonoBehaviour
     // Start is called before the first frame update
 	public int speed = 8;
     bool onCollision = false;
-    public int jumpForce = 3;
+    public int jumpForce = 25;
     Rigidbody2D rb;
     void Awake()
     {
@@ -19,18 +19,27 @@ public class movementP1 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(rb.velocity.x > 8)
+        if(rb.velocity.x > 8f)
             rb.velocity.Set(8f, rb.velocity.y);
         if(transform.position.y < -10)
             transform.Translate(Vector3.up * 20, Space.World);
         if(Input.GetKey(KeyCode.UpArrow) && onCollision)
-            rb.AddForce(transform.up, ForceMode2D.Impulse);
+            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+            
         if(Input.GetKey(KeyCode.LeftArrow))
-            rb.AddForce(-transform.right * speed);
+        {
+            rb.velocity = new Vector2 (-speed, rb.velocity.y);
+            transform.rotation = new Quaternion(0,180f,0,0);
+        }
+            
         if(Input.GetKey(KeyCode.RightArrow))
-            rb.AddForce(transform.right * speed);
+        {
+            rb.velocity = new Vector2 (speed, rb.velocity.y);
+            transform.rotation = new Quaternion(0,0,0,0);
+        }
+            
     }
      void OnCollisionEnter2D(Collision2D other) {
         onCollision = true;
