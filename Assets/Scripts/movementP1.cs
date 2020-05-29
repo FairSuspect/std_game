fe8f33914b4847253817ed74acbcf75622465ee0
Vyacheslav <slava.klevleev@gmail.com>
2020-05-29 15:37:42 +0300

    Fix: Player 1 Physics

--- Assets/Scripts/movementP1.cs
+++ Assets/Scripts/movementP1.cs
@@ -2,49 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movementP1 : MonoBehaviour
-{
+public class movementP1 : MonoBehaviour {
     // Start is called before the first frame update
-	public int speed = 8;
+    public int speed = 8;
     bool onCollision = false;
-    public int jumpForce = 25;
+    public float jumpForce = 10.0f;//Через переменную не работает
     Rigidbody2D rb;
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    void Awake () {
+        rb = GetComponent<Rigidbody2D> ();
     }
-    void Start()
-    {
-    //    Debug.Log(GetComponent<Score>());
+    void Start () {
+        //    Debug.Log(GetComponent<Score>());
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        if(rb.velocity.x > 8f)
-            rb.velocity.Set(8f, rb.velocity.y);
-        if(transform.position.y < -10)
-            transform.Translate(Vector3.up * 20, Space.World);
-        if(Input.GetKey(KeyCode.UpArrow) && onCollision)
-            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
-            
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.velocity = new Vector2 (-speed, rb.velocity.y);
-            transform.rotation = new Quaternion(0,180f,0,0);
+    void FixedUpdate () {
+        if (Mathf.Abs(rb.velocity.x) > 8.0f) {
+            rb.velocity = new Vector3 (rb.velocity.x * 0.65f, rb.velocity.y, 0f);
         }
-            
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.velocity = new Vector2 (speed, rb.velocity.y);
-            transform.rotation = new Quaternion(0,0,0,0);
+
+        //вверх
+        if (Input.GetKey (KeyCode.UpArrow) && onCollision) {
+            rb.velocity = new Vector3 (rb.velocity.x, 12.5f, 0f);
         }
-            
+
+        //вправо и влево
+        if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow)) {
+            float x = Input.GetAxis ("Horizontal");
+            Vector3 move = new Vector3 (x * 12.5f, rb.velocity.y, 0f);
+            rb.velocity = move;
+
+            transform.rotation =  x > 0 ? new Quaternion (0, 0, 0, 0) : new Quaternion (0, 270f, 0, 0);
+        }
+
     }
-     void OnCollisionEnter2D(Collision2D other) {
+
+    void OnCollisionEnter2D (Collision2D other) {
         onCollision = true;
     }
-    void OnCollisionExit2D(Collision2D other) {
+    void OnCollisionExit2D (Collision2D other) {
         onCollision = false;
     }
 }
